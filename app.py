6e8f8ab42030f1fefa53312593def5fcc0f175a8
Luis Correa <correaleyval@gmail.com>
2020-01-30 20:23:40 -0500

    Using cuba-weather-redcuba

---
+++ app.py
@@ -0,0 +1,23 @@
+import config
+
+from flask import Flask, request
+
+from .bot import bot
+
+server = Flask(__name__)
+
+@server.route('/' + config.token, methods=['POST'])
+def getMessage():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(
+            request.stream.read().decode("utf-8")
+        )]
+    )
+
+    return "!", 200
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://cuba-weather.herokuapp.com/' + config.token)
+    return "!", 200
---
+++ bot_response.py
@@ -0,0 +1,44 @@
+def welcome_message(name):
+    welcome_msg = "Hola {} enviame el nombre de una localidad de Cuba para conocer su estado meteorológico"
+
+    return welcome_msg.format(name)
+
+
+def weather_message(weather):
+    res_msg ="""
+    <strong>🌐 {}</strong>\n
+    📅 {}\n
+    <strong>{}</strong>\n
+    <strong>🌡 Temperatura:</strong> {}°C\n
+    <strong>💧 Humedad:</strong> {}%\n
+    <strong>Presión atmosférica:</strong> {} hpa\n\n
+    <strong>🌬 Vientos: </strong>\n
+    <strong>Velocidad: </strog>{} Km/h\n
+    <strong>Direccion: </strong>{}\n
+    """
+
+    emoji_dict = {
+        'despejado': '☀️'
+        'ligera': '🌦'
+        'nublado': '🌥'
+        'intensa': '🌨'
+    }
+
+    gemoji = '⛅️'
+
+    for k in emoji_dict.keys():
+        if k in weather.descriptionWeather.lower():
+            gemoji = emoji_dict[k]
+
+    return res_msg.format(
+        weather.cityName,
+        str(weather.dt.date),
+        gemoji + ' ' + weather.descriptionWeather,
+        weather.temp,
+        weather.humidity,
+        weather.pressure,
+        weather.windVelocity,
+        weather.windDirectionDescription,
+    )
+
+    
